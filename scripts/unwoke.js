2a3f06f52a9f02e6e283bd3ec0eed9c7a0b55979
NikkieDev <dev.nikkie@gmail.com>
2025-01-17 22:01:08 +0100

    ui ready

--- scripts/unwoke.js
+++ scripts/unwoke.js
@@ -1,7 +1,37 @@
-setTimeout(() => {
-  const sampleItems = [".hero__text-sample", "[contenteditable]", ".text-container"];
+const previewItems = '.hero__text-sample, [contenteditable]';
+const customPreview = '.text-container'
 
-  document.querySelectorAll(sampleItems.join(', ')).forEach(sample => {
-    sample.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-  })
-}, 500)
+const availableTexts = {
+  lorem: 1,
+  fox: 2,
+};
+
+let storedText;
+
+chrome.storage.local.get(['text_used']).then(result => {
+  storedText = parseInt(result.text_used);
+});
+
+setTimeout(() => {
+  const pageText = document.querySelectorAll('[contenteditable], .text-container, .hero__text-sample');
+  const fox = "The quick brown fox jumps over the lazy dog ";
+  const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
+  pageText.forEach(textField => {
+    if (storedText == availableTexts.fox) {
+      const fontSize = parseInt(window.getComputedStyle(textField, null).getPropertyValue('font-size'));
+      textField.textContent = fontSize <= 24 ? fox.repeat(20) : fox.repeat(3);
+    } else {
+      textField.textContent = fontSize <= 24 ? lorem.repeat(3) : lorem;
+    }
+  });
+}, 500);
+
+// setTimeout(() => {
+  
+//   const sampleItems = [".hero__text-sample", "[contenteditable]", ".text-container"];
+
+//   document.querySelectorAll(sampleItems.join(', ')).forEach(sample => {
+//     sample.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+//   })
+// }, 500)
